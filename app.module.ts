bd5ea6d5b4ba49e8db988edf2ab4e75f73c5b7c5
NishuGoel <nishu0505@gmail.com>
2018-12-10 01:07:00 +0530

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first.component';
+import { SecondComponent } from './second.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, FirstComponent, SecondComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
